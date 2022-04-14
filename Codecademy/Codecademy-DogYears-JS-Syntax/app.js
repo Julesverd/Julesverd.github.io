c1c2345dc7196d99eb363620a94a779c883c7d53
Jkirb <julesverd@gmail.com>
2022-04-14 16:44:04 -0400

    Codecademy Convert Human age to DogYears-JS Syntax

---
+++ Codecademy/Codecademy-DogYears-JS-Syntax/app.js
@@ -0,0 +1,16 @@
+const myAge = 25;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2;
+laterYears *= 4;
+console.log(earlyYears);
+console.log(laterYears);
+
+// Adding earlyYears and laterYears together, and store that in a variable named myAgeInDogYears.
+const myAgeInDogYears = earlyYears + laterYears;
+
+'Jules Verdadero' .toLowerCase();
+
+console.log(`My name is ${'Jules Verdadero'}. I am ${myAgeInDogYears} years old in dog years.`);
+
+// You can convert any human age to dog years. Try changing myAge and see what happens
