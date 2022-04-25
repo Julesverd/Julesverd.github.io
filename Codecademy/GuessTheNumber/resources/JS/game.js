2053e8be882d348bce11a5e9fbe0f20395c4bc95
Jkirb <julesverd@gmail.com>
2022-04-25 00:57:17 -0400

    Practice Number Guesser Project

---
+++ Codecademy/GuessTheNumber/resources/JS/game.js
@@ -0,0 +1,140 @@
+let target;
+
+const humanGuessInput = document.getElementById('human-guess');
+
+const roundNumberDisplay = document.getElementById('round-number');
+
+const computerGuessDisplay = document.getElementById('computer-guess');
+const humanScoreDisplay = document.getElementById('human-score');
+const computerScoreDisplay = document.getElementById('computer-score');
+const targetNumberDisplay = document.getElementById('target-number');
+const computerWinsDisplay = document.getElementById('computer-wins');
+
+const guessButton = document.getElementById('guess');
+const nextRoundButton = document.getElementById('next-round')
+
+guessButton.addEventListener('click', () => {
+  // Generate the target value
+  target = generateTarget();
+  // Retrieve the player's guess
+  const currentHumanGuess = humanGuessInput.value;
+  // Make a random 'computer guess'
+  const computerGuess = Math.floor(Math.random() * 10);
+
+  // Display the computer guess and the target
+  computerGuessDisplay.innerText = computerGuess;
+  targetNumberDisplay.innerText = target;
+  
+  // Determine if the human or computer wins:
+  const humanIsWinner = compareGuesses(currentHumanGuess, computerGuess, target)
+  const winner = humanIsWinner ? 'human' : 'computer'
+
+  // Update the correct score:
+  updateScore(winner);
+
+  // Display the winner
+  if (humanIsWinner) {
+    guessButton.innerText = 'You Win!!!!!';
+    guessButton.classList.toggle('winning-text')
+  } else {
+    computerWinsDisplay.innerText = 'Computer Wins!!!';
+  }
+
+  // winnerDisplay.innerText = humanIsWinner ? 'You win!' : 'Computer wins!';
+
+  // Display the current scores:
+  humanScoreDisplay.innerText = humanScore;
+  computerScoreDisplay.innerText = computerScore;
+  
+  // Set the correct disabled state for the buttons
+  guessButton.setAttribute('disabled', true)
+  nextRoundButton.removeAttribute('disabled');
+});
+
+nextRoundButton.addEventListener('click', () => {
+  // Increase the round number
+  advanceRound();
+  // Display the new round number
+  roundNumberDisplay.innerText = currentRoundNumber;
+
+  // Set the correct disabled state for the buttons
+  nextRoundButton.setAttribute('disabled', true);
+  guessButton.removeAttribute('disabled');
+
+  // Reset the guess input box and the target number display:
+  targetNumberDisplay.innerText = '?';
+  guessButton.innerText = 'Make a Guess';
+  humanGuessInput.value = '';
+  computerGuessDisplay.innerText = '?';
+  computerWinsDisplay.innerText = '';
+  guessButton.classList.remove('winning-text');
+});
+
+const addButton = document.getElementById('add');
+const subtractButton = document.getElementById('subtract');
+
+addButton.addEventListener('click', () => {
+  humanGuessInput.value = +humanGuessInput.value + 1;
+  handleValueChange(humanGuessInput.value);
+});
+
+subtractButton.addEventListener('click', () => {
+  humanGuessInput.value = +humanGuessInput.value - 1;
+  handleValueChange(humanGuessInput.value);
+});
+
+const handleValueChange = value => {
+  if (value > 0 && value <= 9) {
+    subtractButton.removeAttribute('disabled');
+    addButton.removeAttribute('disabled');
+  } else if (value > 9) {
+    addButton.setAttribute('disabled', true);
+  } else if (value <= 0) {
+    subtractButton.setAttribute('disabled', true);
+  }
+}
+
+humanGuessInput.addEventListener('input', function(e) {
+  handleValueChange(e.target.value);
+});
+
+
+/*
+
+Challenge Project: Number Guesser
+
+1. Create a generateTarget() function. This function should return a random integer between 0 and 9.
+
+The purpose of this function is to be called at the start of each new round in order to generate the new secret target number.
+
+2. 
+Create a compareGuesses() function. This function:
+
+Has three parameters representing the user (human) guess, a computer guess, and the secret target number to be guessed.
+Determines which player (human or computer) wins based on which guess is closest to the target. If both players are tied, the human user should win.
+Return true if the human player wins, and false if the computer player wins.
+The purpose of this function is to be called each round to determine which guess is closest to the target number.
+
+3.
+Create an updateScore() function. This function:
+
+Has a single parameter. This parameter will be a string value representing the winner.
+Increases the score variable (humanScore or computerScore) by 1 depending on the winner passed in to updateScore. The string passed in will be either 'human' or 'computer'.
+Does not need to return any value.
+The purpose of this function is to be used to correctly increase the winner’s score after each round.
+
+4.
+Create an advanceRound() function. This function should increase the value of currentRoundNumber by 1.
+
+The purpose of this function is to be used to update the round number after each round.
+
+After completing advanceRound(), your Number Guesser game should be fully operational. You should be able to make guesses, see your or the computer score increase correctly, move to the next round, and see the correct round displayed.
+
+5.
+Test that your code is working properly by invoking your newly written functions within script.js with sample inputs. You can delete this code once you’re convinced that everything is working as it should.
+
+Great work! If you’d like to see the solution, move to the next task. If you’d like to extend your project on your own, you could consider the following:
+
+You probably calculated the distance from the computer guess to the target and from the human guess to the target. Move this into a separate getAbsoluteDistance() function that takes two numbers and returns the distance, and then use that inside your compareGuesses() function.
+Add functionality to check whether the user guess is between 0 and 9 and alert() the user that their number is out of range. It’s not possible to set a number outside this range with the + and = buttons, but users can do so by typing directly in the input field.
+
