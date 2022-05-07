2eb8bc1801f6fc98e3aa1414ac6a75ba2350a5e2
Jkirb <julesverd@gmail.com>
2022-05-07 15:23:55 -0400

    Personal Portfolio Project - some changes, nav x

--- Codecademy/Personal-Portfolio-Project/resource/JS/my.js
+++ Codecademy/Personal-Portfolio-Project/resource/JS/my.js
@@ -1,1 +1,19 @@
-const toggleButton = document.getElementsByClassName('toggle')
+const toggleButton = document.getElementsByClassName('hamburger')[0]
+const navbarLinks = document.getElementsByClassName('navigation-items')[0]
+
+toggleButton.addEventListener('click', () => {
+    navbarLinks.classList.toggle('active')
+})
+
+
+const timeContainer = document.getElementById("time-container");
+const BIRTH_DAY = "2-02-2022";
+const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
+setInterval(() => {
+  const now = new Date();
+  const difference = Math.floor(
+    (now.getTime() - BIRTH_DAY_DATE.getTime()) / 1000
+  );
+  timeContainer.innerText = intlNumberFormatter.format(difference);
+}, 1000);
