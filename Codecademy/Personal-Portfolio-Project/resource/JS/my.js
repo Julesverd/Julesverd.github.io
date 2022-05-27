e033a9ffac82ffb651248f17b286962054a0e230
Jkirb <julesverd@gmail.com>
2022-05-27 02:43:20 -0400

    Codecademy Personal Portfolio changes

--- Codecademy/Personal-Portfolio-Project/resource/JS/my.js
+++ Codecademy/Personal-Portfolio-Project/resource/JS/my.js
@@ -1,11 +1,3 @@
-const toggleButton = document.getElementsByClassName('hamburger')[0]
-const navbarLinks = document.getElementsByClassName('navigation-items')[0]
-
-toggleButton.addEventListener('click', () => {
-    navbarLinks.classList.toggle('active')
-})
-
-
 const timeContainer = document.getElementById("time-container");
 const BIRTH_DAY = "2-02-2022";
 const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
@@ -17,3 +9,4 @@ setInterval(() => {
   );
   timeContainer.innerText = intlNumberFormatter.format(difference);
 }, 1000);
+
